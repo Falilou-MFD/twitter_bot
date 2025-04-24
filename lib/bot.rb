aec97648a6ffec37c3393fa5238e4709e2427f52
Falilou-MFD <157077834+Falilou-MFD@users.noreply.github.com>
2025-04-24 22:37:22 +0000

    Create bot.rb

---
+++ lib/bot.rb
@@ -0,0 +1,13 @@
+require 'twitter'
+require 'dotenv/load'
+
+class Bot
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_SECRET']
+    end
+  end
+end
